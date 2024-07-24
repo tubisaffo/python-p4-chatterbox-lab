b15591383afdb5661f78a15ccfefc2af701214c9
tubisaffo <tubisaffo@gmail.com>
2024-07-24 10:36:06 +0300

    chatterbox

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -14,13 +15,55 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([{
+        'id': message.id,
+        'body': message.body,
+        'username': message.username,
+        'created_at': message.created_at,
+        'updated_at': message.updated_at
+    } for message in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(
+        body=data['body'],
+        username=data['username']
+    )
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify({
+        'id': new_message.id,
+        'body': new_message.body,
+        'username': new_message.username,
+        'created_at': new_message.created_at,
+        'updated_at': new_message.updated_at
+    }), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+    if 'body' in data:
+        message.body = data['body']
+    db.session.commit()
+    return jsonify({
+        'id': message.id,
+        'body': message.body,
+        'username': message.username,
+        'created_at': message.created_at,
+        'updated_at': message.updated_at
+    })
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
 
 if __name__ == '__main__':
     app.run(port=5555)
